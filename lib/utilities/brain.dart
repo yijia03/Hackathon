001aa808586073f538395f7bfbec792f5fb42a25
yijia03 <77756485+yijia03@users.noreply.github.com>
2022-05-14 14:30:21 -0500

    comments

--- lib/utilities/brain.dart
+++ lib/utilities/brain.dart
@@ -2,8 +2,10 @@
 import 'package:papyrus/exceptions.dart';
 import 'package:papyrus/utilities/set.dart';
 
+///A collection of all the sets the user has created, in alphabetical order by name.
 class Brain /*extends ChangeNotifier*/ {
    final List<WordSet> _sets = [];
+   ///Creates an empty set with the given setName and adds it to the list.
    void create(String setName) {
       int i;
       for (i = 0; i < _sets.length; i++) {
@@ -17,7 +19,7 @@ class Brain /*extends ChangeNotifier*/ {
       }
       _sets.insert(i, WordSet(setName));
    }
-
+   ///Inserts a preexisting set to the list.
    void insert(WordSet set) {
       int i;
       for (i = 0; i < _sets.length; i++) {
@@ -33,8 +35,9 @@ class Brain /*extends ChangeNotifier*/ {
    }
 
    @override
+   ///String representation of the collection.
    String toString() => _sets.toString();
-
+   ///Finds the position of a set using its name. Returns -1 if the set isn't part of the collection.
    int _getIndex(String setName) {
       int min = 0;
       int max = _sets.length;
@@ -52,7 +55,7 @@ class Brain /*extends ChangeNotifier*/ {
       }
       return -1;
    }
-
+   ///Deletes a set in the collection and returns it. Throws NotFoundException when trying to delete a set not in the collection.
    WordSet delete(String setName) {
       int index = _getIndex(setName);
       if (index == -1){
@@ -62,7 +65,7 @@ class Brain /*extends ChangeNotifier*/ {
       _sets.removeAt(index);
       return set;
    }
-
+   ///Changes the name of an existing set by deleting it and then reinserting it. Throws NotFoundException when trying to rename a set not in the collection.
    void changeName(String setName, String newName) {
       WordSet set = delete(setName);
       set.setName(newName);
--- lib/utilities/set.dart
+++ lib/utilities/set.dart
@@ -1,8 +1,9 @@
 import 'package:papyrus/exceptions.dart';
 import 'package:papyrus/utilities/note_card.dart';
 
+///A single set of words, always in alphabetical order.
 class WordSet {
-  //Always ordered in alphabetical order
+  //Always ordered in alphabetical order.
   final List<NoteCard> _lst = [];
   String _name;
   WordSet(this._name);
@@ -11,7 +12,9 @@ class WordSet {
   String getName() => _name;
   void setName(String name) => _name = name;
   @override
+  ///String representation of the word set.
   String toString() => '$_name: $_lst';
+  ///Inserts card into the set in alphabetical order.
   void insert(NoteCard card){
     int i;
     for (i = 0; i < _lst.length; i++) {
@@ -25,6 +28,7 @@ class WordSet {
     }
     _lst.insert(i, card);
   }
+  ///Deletes a card from the list.
   void delete(NoteCard card) {
     _lst.remove(card);
   }
