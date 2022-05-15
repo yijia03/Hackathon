324d5cdb7c7e864aaa3e1ec89f5b5271fcbad2ef
yijia03 <77756485+yijia03@users.noreply.github.com>
2022-05-14 22:25:00 -0500

    maybe made changes?

--- lib/widgets/set_card.dart
+++ lib/widgets/set_card.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:papyrus/screens/practice_screen.dart';
+import 'package:papyrus/utilities/brain.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/editor_screen.dart';
 
 class SetCard extends StatelessWidget {
   String _name;
@@ -6,6 +11,31 @@ class SetCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text(_name), color: Colors.red,);
+    Brain b = Provider.of<Brain>(context, listen: false);
+    return Container(
+      child: Row(
+        children: [
+          Text(_name),
+          IconButton(
+              onPressed: () {
+                b.select(_name);
+                Navigator.pushNamed(context, PracticeScreen.id);
+              },
+              icon: Icon(Icons.play_arrow)),
+          IconButton(
+              onPressed: () {
+                b.select(_name);
+                Navigator.pushNamed(context, EditorScreen.id);
+              },
+              icon: Icon(Icons.create)),
+          IconButton(
+              onPressed: () {
+                b.delete(_name);
+              },
+              icon: Icon(Icons.delete)),
+        ],
+      ),
+      color: Colors.red,
+    );
   }
 }
