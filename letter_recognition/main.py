9dacc1393d7b87de642713f200676206f260ea7c
Yaokai Yang <yaokai.y@gmail.com>
2022-05-15 10:18:46 -0500

    character reader completed

--- letter_recognition/main.py
+++ letter_recognition/main.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # key for labels to digits & letters
 key = np.array(['0','1','2','3','4','5','6','7','8','9',
-       'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+       'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
        'a','b','d','e','f','g','h','n','q','r','t'])
 
 class LetterReader:
@@ -36,16 +36,16 @@ class LetterReader:
     # takes in 28x28 greyscale picture as a 784 element list (domain of 0-1), outputs a 47 element list of confidence in each possible label 
     def recog(self, arr=[]):
         arr = np.array(arr).flatten()
-        arr = np.append(arr, [1])     # add bias node
 
+        arr = np.append([1], arr)     # add bias node
         output = np.matmul(arr, self.theta1_T)
-        output = np.append(output, [1])
+        output = self.sigmoid(output)
+
+        output = np.append([1], output)
+        output = np.matmul(output, self.theta2_T)
         output = self.sigmoid(output)
         
-        output = np.matmul(output, self.theta2_T)
-        output = np.append(output, [1])
-        output = self.sigmoid(output)
-
+        output = np.append([1], output)
         output = np.matmul(output, self.theta3_T)
         output = self.sigmoid(output)
 
@@ -55,24 +55,27 @@ class LetterReader:
     def sigmoid(self, arr):
         output = []
         for element in arr:
-            output.append(1 / (1 + np.exp(-element)))
+            output.append(1.0 / (1.0 + np.exp(-element)))
 
         output = np.array(output)
         return output
 
-letterReader = LetterReader()
+## TESTING PURPOSES ##
+# letterReader = LetterReader()
 
-input = []
-with open('AI_Training/Output/testInput.csv', 'r') as f:
-    reader = csv.reader(f, quoting = csv.QUOTE_NONNUMERIC)
-    for line in reader:
-        input.append(line[:])
-input = np.true_divide(input, 255)
+# input = []
+# with open('AI_Training/Output/testInput.csv', 'r') as f:
+#     reader = csv.reader(f, quoting = csv.QUOTE_NONNUMERIC)
+#     for line in reader:
+#         input.append(line[:])
+# input = np.true_divide(input, 255)
 
-output = letterReader.recog(input)
+# output = letterReader.recog(input)
 
-# gets thhe indicies of the top 3 most likely characters 
-outindex = np.argpartition(output, -3)[-3:]
-outindex = outindex[np.argsort(output[outindex])]
-
-print(key[output])
+# # gets thhe indicies of the top 3 most likely characters, the last element is the most likely
+# outindex = np.argpartition(output, -3)[-3:]
+# outindex = outindex[np.argsort(output[outindex])]
+# print(key[46])
+# print(outindex)
+# print(key[outindex])
+#
