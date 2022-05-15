21a19b6c9154d5e4db879691285af195f37f5f5e
Jeremy Chung <63564359+221ChungJ@users.noreply.github.com>
2022-05-14 20:38:54 -0500

    toongs

--- lib/screens/editor_screen.dart
+++ lib/screens/editor_screen.dart
@@ -1,20 +1,96 @@
 import 'package:flutter/material.dart';
 import 'package:papyrus/utilities/brain.dart';
 import 'package:provider/provider.dart';
+import 'package:papyrus/constants.dart';
 
-class EditorScreen extends StatelessWidget {
+class EditorScreen extends StatefulWidget {
   const EditorScreen({Key? key}) : super(key: key);
   static const String id = "Editor Screen";
+
+  @override
+  State<EditorScreen> createState() => _EditorScreenState();
+}
+
+class _EditorScreenState extends State<EditorScreen> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Consumer<Brain>(builder: (context, brain, child) {
       return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-        ),
-        body: Container(
-
+        resizeToAvoidBottomInset: false,
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 30, horizontal: 10),
+                  child: Text(
+                    'Title: ',
+                    style: kSetTextStyle,
+                  ),
+                ),
+                Container(
+                  width: 250,
+                  child: TextField(
+                    style: kTextFieldTextStyle,
+                    autocorrect: true,
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(vertical: 0),
+                      isDense: true,
+                      hintText: 'Enter a title',
+                      hintStyle: kTextFieldHintStyle,
+                      enabledBorder: InputBorder.none,
+                    ),
+                    controller: _controller,
+                    onSubmitted: (String input) {
+                      brain.getCurr().setName(input);
+                      print(brain.getCurr().toString());
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.bottomEnd,
+              children: <Widget>[
+                Container(
+                  height: MediaQuery.of(context).size.height - 275,
+                  child: ListView(),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(15),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          brain.create();
+                          Navigator.pushReplacementNamed(
+                              context, EditorScreen.id);
+                        },
+                        child: Icon(Icons.add),
+                        style: kAddButtonStyle,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       );
     });
